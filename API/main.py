287dbd53974f5c5646927fc7d68c3669b4e0c27f
Lukas Pfeiffer <lukas.pfeiffer99@gmail.com>
2020-07-18 18:08:05 +0200

    Added drinks per user

--- API/main.py
+++ API/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
@@ -26,6 +28,22 @@ app.add_middleware(
 @app.get("/drinks")
 def list_drinks(db: Session = Depends(get_db)):
     return db.query(models.Drink).all()
+
+
+@app.get("/drinks/{username}")
+def drinks_for_user(username: str, db: Session = Depends(get_db)):
+    db_drinks: List[models.Drink] = db.query(models.Drink).filter(models.Drink.name == username).all()
+    drinks_per_user = dict()
+    for drink in db_drinks:
+        if drink.name not in drinks_per_user.keys():
+            drinks_per_user[drink.name] = dict()
+
+        if drink.drink in drinks_per_user[drink.name].keys():
+            drinks_per_user[drink.name][drink.drink] += 1
+        else:
+            drinks_per_user[drink.name][drink.drink] = 1
+
+    return drinks_per_user
 
 
 @app.post("/drinks")
