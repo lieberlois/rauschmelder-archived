5a4d8b41df4fd1d67bb2d809d1db797c19ddff52
Lukas Pfeiffer <lukas.pfeiffer99@gmail.com>
2020-07-18 16:09:27 +0200

    Initial API Commit

---
+++ API/main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def example_route():
+    return {"Schoko": "Crew"}
+
+
+if __name__ == '__main__':
+    # Use this for debugging purposes only, otherwise start with "uvicorn main:app --reload"
+    uvicorn.run(app, host="127.0.0.1", port=8000)
